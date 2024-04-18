d445671288f86b082265ef43ea4d8c8c5df377b0
Karlana <kkarlanalimaa@gmail.com>
2024-04-17 21:18:59 -0300

    commit retrativo

--- modulos/pesquisa_digital.py
+++ modulos/pesquisa_digital.py
@@ -3,8 +3,8 @@ import datetime as dt
 
 class PesquisaDigital:
     def __init__(self):
-        # Criando dataFrame vazio
-        self.df = pd.DataFrame()
+        #Lista para armazenar as respostas do user
+        self.respostas = []
         # Lista das perguntas para o user
         self.lista_perguntas = [
             'Você acredita que a falta de acesso à tecnologia contribui para a desigualdade social no Brasil?',
@@ -14,7 +14,7 @@ class PesquisaDigital:
             'Você acha que todos têm acesso igual à educação digital no Brasil?',
             'Você acha que o governo está fazendo o suficiente para reduzir a diferença no acesso à tecnologia?'
         ] 
-    # Metodo para mostrar o meunu pardão.
+    # Metódo para mostrar o menu padrão.
     def menu_pesquisa(self):
         print(f'''
         =================================================
@@ -26,40 +26,54 @@ class PesquisaDigital:
         ======                                     ======
         =================================================  
         -> ''')
-    # Metodo para realizar a pesquisa com o user
+
+    # Metódo para realizar a pesquisa com o user
     def fazer_pesquisa(self):
         while True:
-            idade = int(input('Digite sua idade: '))
-            # Caso a idade do user seja 0, o codigo vai parar
-            if idade == 0:
-                self.df.to_csv('pesquisa_digital.csv', index=False, sep=';')
-                print(self.df)
-                print("Dados salvos com sucesso em 'pesquisa_digital.csv'")
-                break
+            try:
+                idade = int(input('Digite sua idade: '))
+                # Caso a idade do user seja 0, o codigo vai parar
+                if idade == 0:
+                    if not self.respostas:
+                        print('Nenhuma pesquisa realizada.')
+                    else:
+                        df = pd.DataFrame(self.respostas)
+                        df.to_csv('pesquisa_digital.csv', index=False)
+                        print(df)
+                        print("Dados salvos com sucesso em 'pesquisa_digitalcsv'")
+                    break
 
-            # caso não, ele realiza a pesquisa.
-            elif idade > 0:
-                genero = input('Digite seu gênero: ')
-                respostas = []
-                for pergunta in self.lista_perguntas:
-                    print(pergunta)
-                    resposta = int(input(self.menu_pesquisa()))
-                    if resposta == 1:
-                        resposta = 'SIM'
-                        respostas.append(resposta)
-                    elif resposta == 2:
-                        resposta = 'NAO'
-                        respostas.append(resposta)
-                    elif resposta == 3:
+                # Caso não, ele realiza a pesquisa.
+                elif idade > 0:
+                 genero = input('Digite seu gênero: ')
+                 respostas = {}
+                 for i, pergunta in enumerate(self.lista_perguntas):
+                     print(pergunta)
+                     resposta = input(self.menu_pesquisa())
+                     while resposta not in ['1', '2', '3']:
+                         print('Por favor, escolha uma opção válida.')
+                         print(pergunta) # Repete a pergunta
+                         resposta = input(self.menu_pesquisa())
+                     if resposta == 1:
+                         resposta = 'SIM'
+                     elif resposta == 2:
+                         resposta = 'NAO'
+                     elif resposta == 3:
                         resposta = 'NAO SEI RESPONDER'
-                        respostas.append(resposta)
+                     respostas[f'resposta_{i+1}'] = resposta
+                respostas['idade'] = idade
+                respostas['genero'] = genero.upper()
                 
                 # Data e hora que a resposta foi escrita no sistema.
                 hora_atual = dt.datetime.now()
                 data_hora_resposta = hora_atual.strftime('%d/%m/%Y %H:%M')
-                        
-                # Criando uma nova linha ao dataFrame
-                nova_linha = {'idade': idade, 'genero': genero.upper(), 'resposta_1':respostas[0], 'resposta_2':respostas[1], 'resposta_3':respostas[2], 
-                          'resposta_4':respostas[3], 'resposta_5':respostas[4], 'resposta_6':respostas[5], 'data/hora resposta': data_hora_resposta}
-                # Adicionando a nova linha ao dataframe.
-                self.df = self.df._append(nova_linha, ignore_index=True)
+                respostas['data_hora_resposta'] = data_hora_resposta
+
+                self.respostas.append(respostas)
+
+            except ValueError:
+                print('Por favor, digite uma idade válida')
+
+if __name__ == '__main__':
+    pesquisa = PesquisaDigital()
+    pesquisa.fazer_pesquisa()
